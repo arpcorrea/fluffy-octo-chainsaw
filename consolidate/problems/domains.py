5b7d93f4dfe87fb1a175f030e47600a5dac869fa
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-24 14:20:16 -0400

    Create domains.py

---
+++ consolidate/problems/domains.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class RectangularDomain:
+
+    def __init__(self, name, corner0, corner1, MaterialProperties):
+        self.x0 = float(corner0[0])
+        self.y0 = float(corner0[1])
+        self.x1 = float(corner1[0])
+        self.y1 = float(corner1[1])
+        self.Lx = self.x1 - self.x0
+        self.Ly = self.y1 - self.y0
+        self.name = name
+        self.MaterialProperties = MaterialProperties
+        
+       
+
+    def test(self, point):
+        if point[0] >= self.x0 and point[0] <= self.x1 and point[1] >= self.y0 and point[1] <= self.y1:
+            return True
+        else:
+            return False
+
+ 
+
+    def set_field_init_value(self, field_dict):
+        
+        for key, value in field_dict.items():
+            self.initial_fields[key] = float(value)
+            
+    
+    def set_material_properties(self, mat):
+        import pdb; pdb.set_trace()
+        for key, value in mat:
+            self.MaterialProperties[key] = float (value)
